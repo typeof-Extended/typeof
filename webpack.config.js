552c8c4dea58b6aaa5f3ba1517c2a2a1bc8bca37
JTak <jasonytak@gmail.com>
2017-08-25 10:57:59 -0700

    Boiler-plate

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+entry: './client/index.js',
+output: {
+  path: path.join(__dirname, '/client'),
+  publicPath: '/client',
+  filename: 'bundle.js'
+},
+  module: {
+    loaders: [
+      {
+        test: /jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-class-properties']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ]
+  }
+}
