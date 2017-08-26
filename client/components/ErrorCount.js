4df2a767ab7ab1a238870c9b4c34fef5e3ecb6b2
JTak <jasonytak@gmail.com>
2017-08-26 13:52:47 -0700

    Update Functionality

--- client/components/ErrorCount.js
+++ client/components/ErrorCount.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ErrorCount extends Component {
   render() {
     return (
-      <div>
+      <div id="error">
         Error Count: {this.props.errors}
       </div>
     );
