38579f689d5e2c33763a65a5d032aa697e075898
Gian Cui <gianthedesigner@gmail.com>
2017-08-28 23:16:13 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const pg = require('pg');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const stringController = require('./stringController/stringController.js');
 
