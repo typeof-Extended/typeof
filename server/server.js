4fb6c56de8bce33af6afb562d6f612a2b802ab75
Kristopherlb <kristopherl.bowles@gmail.com>
2017-08-29 11:27:54 -0700

    added routh for login

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const pg = require('pg');
+const bodyParser = require('body-parser');
+const verifyUser = require('./controllers/verifyUser.js')
+
+const stringController = require('./stringController/stringController.js');
+
 const conString = 'postgres://tjurqsrm:a3EMg4RiFXhLDz5mYScVBDvWlhKP-Ok7@babar.elephantsql.com:5432/tjurqsrm';
-const verifyUser = require('../constrollers/verifyUser.js')
 
 const client = new pg.Client(conString);
 client.connect(function(err) {
@@ -14,20 +18,23 @@ client.connect(function(err) {
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0].theTime);
+    console.log('Connected to database at ', result.rows[0].theTime);
     client.end();
   });
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.post('/login', verifyUser)
+app.post('/getstring', stringController.getLevels, (req, res)=> {
+  res.status(200).send(res.locals.level);
+});
 
-// app.get('/getstring', (req, res)=> {
-
-// });
+app.post('/login', verifyUser.authenticate)
 
 app.use('/dist', express.static('dist'));
 
