8dd8d159f9c34939544b543a688d98ff37157a11
Kristopherlb <kristopherl.bowles@gmail.com>
2017-08-29 11:28:35 -0700

    created verify user authentication. still needs bcrypt

--- server/controllers/verifyUser.js
+++ server/controllers/verifyUser.js
@@ -1,25 +1,30 @@
 const pg = require('pg');
 const conString = 'postgres://tjurqsrm:a3EMg4RiFXhLDz5mYScVBDvWlhKP-Ok7@babar.elephantsql.com:5432/tjurqsrm';
 
-
-
 const client = new pg.Client(conString);
 
-const verifyUser = {}
-
-verifyUser.authenticate = (req, res) => {
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
   }
-  client.query('SELECT * FROM userinfo WHERE username = ' + req.body.username, function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    res.send(result);
-    
-  });
+  else { console.log('connected in controller')}
 })
-}
+
+const verifyUser = {}
+
+verifyUser.authenticate = (req, res) => {
+  client.query("SELECT * FROM userinfo WHERE username = '" + req.body.username + "'", function(err, result) {
+    console.log('this is results: ', result.rows)
+    if(err) {
+      return console.log('error running query', err);
+    }
+    if (result.rows[0].username === req.body.username && result.rows[0].password === req.body.password){
+      res.status(200).send(result.rows)
+
+  };
+  
+})
+};
+
 
 module.exports = verifyUser;
