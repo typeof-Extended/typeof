8508ea28df736a6882f2e3f75bb1fb9d78ed136a
Jon Perera <jon.perera@hotmail.com>
2017-08-29 12:38:24 -0700

    added login and register pages

--- client/components/Views.js
+++ client/components/Views.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import CodeBlock from './CodeBlock';
+import CodeBlock from './CodeBlock.js';
+import Login from './Login.js';
+import Register from './Register.js';
+let xhr = new XMLHttpRequest();
 
 class Views extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      view: 'login',
+    };
   }
 
+  login = (e, formData) => {
+    e.preventDefault();
+    let username = formData.username.value;
+    let password = formData.password.value;
+    xhr.open('POST', 'http://localhost:3000/login');
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        this.changeView('app');
+      }
+    }
+    xhr.send(JSON.stringify({ username, password }));
+  }
 
-  // clean up passing down of props
+  register = (e, formData) => {
+    e.preventDefault();
+    let username = formData.username.value;
+    let email = formData.email.value;
+    let password = formData.password.value;
+    xhr.open('POST', 'http://localhost:3000/createuser');
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        this.setState({view: 'app'});
+      }
+    }
+    xhr.send(JSON.stringify({ username, email, password }));
+  }
+
+  changeView = (view) => {
+    this.setState({ view });
+  } 
+
+
   render() {
 
-    return (
-       <CodeBlock /> 
-    );
+    if (this.state.view === 'login') return <Login login={this.login} changeView={this.changeView} />;
+    else if (this.state.view === 'register') return <Register register={this.register} changeView={this.changeView} />;
+    else if (this.state.view === 'app') return <CodeBlock />;
+
   }  
 }   
 
