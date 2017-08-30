5dce20155aaa9b68a39118579737c5c272c4df26
GianIsAlive <gianthedesigner@gmail.com>
2017-08-29 20:55:28 -0700

    modified stringController to serve up datas in single array

--- server/controllers/stringController.js
+++ server/controllers/stringController.js
@@ -11,17 +11,27 @@ client.connect((err) => {
 const stringController = {};
 
 stringController.getLevels = (req, res, next) => {
-  client.query('SELECT level' + req.body.level + ' FROM stringproblem;', (err, response) => {
+  client.query('SELECT level1, level2, level3 FROM stringproblem;', (err, response) => {
     if (err) {
       console.log(err)
       res.status(400).end();
     };
     //Data is passed down via this res property
     res.locals.level = [];
-
+    let count = 0;
     for (let x = 0; x < response.rows.length; x++) {
       //This is used to create 2nd dimensional array
-      let nested = [response.rows[x]['level' + req.body.level]];
+      let nested = [response.rows[x]['level1']];
+      res.locals.level.push(nested);
+    }
+    for (let x = 0; x < response.rows.length; x++) {
+      //This is used to create 2nd dimensional array
+      let nested = [response.rows[x]['level2']];
+      res.locals.level.push(nested);
+    }
+    for (let x = 0; x < response.rows.length; x++) {
+      //This is used to create 2nd dimensional array
+      let nested = [response.rows[x]['level3']];
       res.locals.level.push(nested);
     }
     next();
